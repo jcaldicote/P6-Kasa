605186b9c160584ecb4425e83bed38ef216ff3e4
jcaldicote <caldicote.j@gmail.com>
2023-07-07 06:38:08 +0200

    test collpase

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { AboutBanner } from "../components/Banner.jsx";
+import "./About.scss";
+import { Collapse } from "../components/Collapse.jsx";
 
 export default function About() {
   return (
     <>
       <AboutBanner />
+      <div className="collapse-container">
+        <Collapse />
+      </div>
     </>
   );
 }
