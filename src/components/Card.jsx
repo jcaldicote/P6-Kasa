9ea3477d6644b1e19274b2dd95f3a46fbc9b0f4b
jcaldicote <caldicote.j@gmail.com>
2023-07-17 16:06:28 +0200

    new update pour le deploiement

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,6 @@
 import PropTypes from "prop-types";
 import { NavLink } from "react-router-dom";
-import "./card.scss";
+import "./Card.scss";
 
 function Card({ picture, title, href }) {
   return (
